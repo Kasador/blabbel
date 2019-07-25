ed7be640a4864766a18d1aeb10b963f3f1d8c807
Kasador <huntershaw23@gmail.com>
2019-07-24 23:14:43 -0500

    INITIAL: first commit, setup workflow, etc.

---
+++ src/Assets/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../Images/logo.png';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/Main/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Assets/Components/Navbar';
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
